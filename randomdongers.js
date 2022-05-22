c0ce404a49d8c79f782aed2bdcbfb042de095a5d
Jerson <105609722+Yersonp@users.noreply.github.com>
2022-05-22 16:01:14 -0500

    Create randomdongers.js

---
+++ randomdongers.js
@@ -0,0 +1,23 @@
+const express = require("express");
+var app = express()
+const utils = require('./utils.js');
+const dongers = require('./dongers.json');
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("API online")
+})
+
+
+   app.get("/randomdongers", async (req, res) => {
+        const donger = utils.randArray(dongers);
+        let response = {
+              status: "success",
+              category: "dongers",
+              result: donger
+           }
+     return res.status(200).send(response)
+                
+             })
